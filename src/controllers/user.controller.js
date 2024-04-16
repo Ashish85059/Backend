337e59af41e373e91aafe718c2411f1a3e5f87ce
Ashish Sharma <ashishsharma903461@gmail.com>
2024-04-16 16:07:53 +0530

    added regster controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,59 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError}from "../utils/ApiError.js"
+import {ApiResponse}from "../utils/ApiResponse.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+
 
 const registerUser=asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"chai aur code"
+    const {fullname,email,username,password}=req.body;
+    // console.log("Emali: ",email)
+
+    if(
+        [fullname,email,username,password].some((feild)=>feild?.trim ==="") // agar ek bhi empty hua toh true return hoga
+    ){
+        throw new ApiError(400,"All feilds are required")
+    }
+
+    const existingUser=User.findOne({
+        $or :[{username},{email}] // agar ek bhi match hua toh return hoga data
     })
+
+    if(existingUser){
+        throw new ApiError(409,"User already exists")
+    }
+
+    const avatarLocalPath=req.files?.avatar[0]?.path
+    const coverImageLocaLPAth=req.files?.coverImage[0]?.path
+    if(!avatarLocalPath){
+        throw new ApiError(400,"avatar file is required")
+    }
+    
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
+    const coverImage=await uploadOnCloudinary(coverImageLocaLPAth);
+    if(!avatar){
+        throw new ApiError(400,"avatar file is required")
+    }
+
+    const user=await User.create({
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+     })   
+
+     const createdUser=await User.findById(user._id).select(
+        "-password -refreshToken"
+     )
+     if(!createdUser){
+        throw new ApiError(500,"Something went Wrong")
+     }
+     return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+     )
+
 })
 
 
